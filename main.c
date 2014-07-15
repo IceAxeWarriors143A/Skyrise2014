4aa3d414feeff271e66a3de0af7956cf7a61f36e
Garrison Taylor <garrisontaylor@gmail.com>
2014-07-15 17:17:01 -0500

    Fix motor controller and while loop

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #pragma config(Motor,    port1,    leftWheels, tmotorNormal, openLoop, reversed)
-#pragma config(Motor,    port2,    rightWheels, tmotorNormal, openLoop)
-#pragma config(Motor,    port3,    middleWheel, tmotorNormal, openLoop)
+#pragma config(Motor,    port10,    rightWheels, tmotorNormal, openLoop)
+#pragma config(Motor,    port3,    middleWheel, tmotorVex393_MC29, openLoop)
 
 int rightX;
 int leftX;
@@ -9,11 +9,14 @@ int leftY;
 
 task main
 {
-    rightX = vexRT[1];
-    rightY = vexRT[1];
-    leftX = vexRT[4];
-    leftY = vexRT[3];
-    motor[leftWheels] = (leftX + rightY)/2;
-    motor[rightWheels] = (-leftX + rightY)/2;
-    motor[middleWheel] = rightX;
+    while(1==1)
+    {
+        rightX = vexRT[1];
+        rightY = vexRT[1];
+        leftX = vexRT[4];
+        leftY = vexRT[3];
+        motor[leftWheels] = (leftX + rightY)/2;
+        motor[rightWheels] = (-leftX + rightY)/2;
+        motor[middleWheel] = rightX;
+    }
 }
