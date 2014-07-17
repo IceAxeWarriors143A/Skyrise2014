a9845d9c43ded2e32e2fdc4abe68f7b2dc41af74
Alex <alex_brinister@yahoo.com>
2014-07-17 14:10:52 -0400

    One joystick tank drive, strafe drive, track drive

--- main.c
+++ main.c
@@ -1,22 +1,37 @@
-#pragma config(Motor,    port1,    leftWheels, tmotorNormal, openLoop, reversed)
-#pragma config(Motor,    port10,    rightWheels, tmotorNormal, openLoop)
-#pragma config(Motor,    port3,    middleWheel, tmotorVex393_MC29, openLoop)
+#pragma config(Motor,    port1,    right, tmotorNormal, openLoop, reversed)
+#pragma config(Motor,    port10,    left, tMotorNormal, openLoop)
+#pragma config(Motor,		 port2,	 middle, tmotorVex393_MC29, openLoop)
+#pragma config(Motor, port3, rightLift, tmotorVex393_MC29, openLoop)
+#pragma config(Motor, port4, leftLift, tmotorVex393_MC29, openLoop)
 
-int rightX;
-int leftX;
-int rightY;
-int leftY;
-
-task main
+void tankDrive(float x, float y)
 {
-    while(1==1)
-    {
-        rightX = vexRT[1];
-        rightY = vexRT[1];
-        leftX = vexRT[4];
-        leftY = vexRT[3];
-        motor[leftWheels] = (leftX + rightY)/2;
-        motor[rightWheels] = (-leftX + rightY)/2;
-        motor[middleWheel] = rightX;
-    }
+		motor[right] = (x - y) / 2;
+		motor[left] = (x + y) / 2;
+}
+
+void driveLift(int speed)
+{
+		motor[rightLift] = speed;
+		motor[leftLift] = -speed;
+}
+
+task main()
+{
+	while(true)
+	{
+		float xSpeed = vexRT[Ch2];
+		float ySpeed = vexRT[Ch1];
+
+		tankDrive(xSpeed, ySpeed);
+
+		if(vexRT[Btn5U])
+			driveLift(127);
+		else if(vexRT[Btn5D])
+			driveLift(-127);
+		else if(vexRT[Btn7L])
+			driveLift(0);
+
+		motor[middle] = vexRT[Ch4];
+	}
 }
